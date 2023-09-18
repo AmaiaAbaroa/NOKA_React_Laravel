c59f68c30c25aec1f613a39742354ed23131fd2c
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-09-18 14:46:29 +0200

    Login-Register-SignInSuccess FrontEnd done - error alerts are missing

---
+++ Noka-FrontEnd/src/components/molecules/LogoutButton/LogoutButton.jsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { APIService } from "../../../services/APIService";
+import ButtonAtom from "../../atoms/ButtonAtom/ButtonAtom";
+
+const LogoutButton = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      // Utiliza APIService para realizar la petición de logout
+      await APIService("/logout", "POST", {});
+
+      // Elimina los datos del usuario del localStorage
+      localStorage.removeItem("user");
+
+      // Redirige a '/login'
+      navigate("/login");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
+
+  return (
+    <ButtonAtom
+      label={"SARTU"}
+      color={"blue"}
+      width={"small"}
+      type={"submit"}
+      action={handleLogout}
+    ></ButtonAtom>
+  );
+};
+
+export default LogoutButton;
--- Noka-FrontEnd/src/pages/Profile.jsx
+++ Noka-FrontEnd/src/pages/Profile.jsx
@@ -1,54 +1,26 @@
-
-
-// export default function HellowLogin(){
-//     return(
-//         <>
-            
-//             <main>
-//                <h1>EGUN ON, MARIPURI! </h1>
-//                 <div>
-                    
-//                     <p>
-//                         Zure kontu barruan zaude.
-//                     </p>
-//                 </div>
-//             </main>
-            
-//         </>
-//     )
-// }
-
-
-import { useAuth } from '../context/AuthContext'; 
-import LogoutButton from '../components/atoms/LogoutButton/LogoutButton';
+import { useAuth } from "../context/AuthContext";
+import LogoutButton from "../components/molecules/LogoutButton/LogoutButton";
 
 export default function Profile() {
   const { user } = useAuth(); // Obtén la información del usuario del contexto
-  console.log(user)
-  
+  console.log(user);
+
   return (
-  
     <>
       <div>
-      {user ? (
-        <main>
-          <h1>EGUN ON, {user ? user.name : 'ONGIETORRI'}!</h1>
-          
-          <div>
-            <p>Zure kontu barruan zaude.</p>
-          </div>
-          <LogoutButton />
-        </main>
-      ) : (
-        <p>Usuario no autenticado</p>
-      )}
-    </div>
-  </>
-  
+        {user ? (
+          <main>
+            <h1>EGUN ON, {user ? user.name : "ONGIETORRI"}!</h1>
+
+            <div>
+              <p>Zure kontu barruan zaude.</p>
+            </div>
+            <LogoutButton />
+          </main>
+        ) : (
+          <p>Usuario no autenticado</p>
+        )}
+      </div>
+    </>
   );
 }
-
-
-
-
-
